e896c087a0e929494243bb5fd2de4837e2cd8077
UntillNIGHT <lengleng9090@gmail.com>
2021-07-24 15:28:13 +0700

    [UPDATE]user generateAuthToken

--- src/models/user.js
+++ src/models/user.js
@@ -75,7 +75,7 @@ userSchema.methods.toJSON = function (){
 //create method for generate json web token(jwt) for user to access api
 userSchema.methods.generateAuthToken = async function (){
     const user = this;
-    const token = jwt.sign({_id: user._id.toString() },'thisismynewcourse');
+    const token = jwt.sign({_id: user._id.toString() },process.env.JWT_SECRET);
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
